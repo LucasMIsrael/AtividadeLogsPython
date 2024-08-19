69f853cd98b519a677e384618399feba232555e3
Lucas Mendes Israel <lukasm.israel@gmail.com>
2024-08-18 22:46:20 -0300

    readme

--- app.py
+++ app.py
@@ -2,13 +2,10 @@ import logging
 from logger_config import setup_logger
 
 def main():
-    # Substitua 'YOUR_LOGGLY_CUSTOMER_TOKEN' pelo seu token real de entrada do Loggly
     customer_token = '0cacd53e-5330-4e40-941d-b51ff6bdfb08'
     
-    # Configura o logger
     logger = setup_logger('app.log', logging.DEBUG, customer_token)
 
-    # Gerando logs em diferentes níveis
     logger.debug('Este é um log de DEBUG: informações detalhadas para diagnóstico.')
     logger.info('Este é um log de INFO: confirmação de que as coisas estão funcionando como esperado.')
     logger.warning('Este é um log de WARNING: algo inesperado aconteceu ou pode acontecer.')
--- logger_config.py
+++ logger_config.py
@@ -17,19 +17,15 @@ def setup_logger(log_file: str, log_level: int = logging.DEBUG, customer_token: 
     logger = logging.getLogger(__name__)
     logger.setLevel(log_level)
 
-    # Cria um handler para escrever os logs em arquivo
     file_handler = logging.FileHandler(log_file)
     file_handler.setLevel(log_level)
 
-    # Formatação dos logs
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
     
-    # Adiciona o handler ao logger
     if not logger.hasHandlers():
         logger.addHandler(file_handler)
     
-    # Configura Loggly se o token for fornecido
     if customer_token:
         loggly_handler = LogglyHandler(customer_token)
         loggly_handler.setLevel(log_level)
